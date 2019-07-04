675f07c8036185b13ad1c67545288bac6d17d3fa
Renato Caminha Juaçaba Neto <rcaminha@etu.unistra.fr>
2019-07-04 17:16:30 +0200

    Novos modos de export

--- exercicios-react/src/components/DoisComponents.jsx
+++ exercicios-react/src/components/DoisComponents.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const CompA = (props) =>
+export const CompA = (props) =>
    <h1> Primeiro diz: {props.valor} </h1>
 
-const CompB = (props) =>
+export const CompB = (props) =>
    <h1> Segundo diz: {props.valor} </h1>
 
-
-export { CompA, CompB }
+export default CompA
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import PrimeiroComponent from './components/PrimeiroComponent'
-// import {CompA, CompB as B} from './components/DoisComponents'
-import MultiElementos from './components/MultiElementos'
+import CompA, {CompB as B} from './components/DoisComponents'
+// import MultiElementos from './components/MultiElementos'
 
 const root = document.getElementById('root')
 
 ReactDOM.render(
    <div>
-      {/* <PrimeiroComponent valor='1' aBcD={2**8}/>
+      {/* <PrimeiroComponent valor='1' aBcD={2**8}/>*/}
       <CompA valor='Sou A'/>
-      <B valor='Sou B'/>*/}
-      <MultiElementos/>
+      <B valor='Sou B'/>
+      {/*<MultiElementos/>*/}
    </div>, root)
