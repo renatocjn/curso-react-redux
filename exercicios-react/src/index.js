e7f4682febe5aff38a478593851d5bf58e92365c
Renato Caminha Juaçaba Neto <rcaminha@etu.unistra.fr>
2019-07-04 17:04:45 +0200

    Dois componentes no mesmo arquivo

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponent from './components/PrimeiroComponent'
+// import PrimeiroComponent from './components/PrimeiroComponent'
+import {CompA, CompB as B} from './components/DoisComponents'
 
 const root = document.getElementById('root')
 
 ReactDOM.render(
    <div>
-      <PrimeiroComponent valor='1' aBcD={2**8}/>
+      {/* <PrimeiroComponent valor='1' aBcD={2**8}/>*/}
+      <CompA valor='Sou A'/>
+      <B valor='Sou B'/>
    </div>, root)
