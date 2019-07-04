27b9d4385f2b68ca570badd5739d0268e36038b8
Renato Caminha Juaçaba Neto <rcaminha@etu.unistra.fr>
2019-07-04 17:24:43 +0200

    Componentes dentro de componentes. Exemplo simples

---
+++ exercicios-react/src/components/FamiliaSilva.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Membro from './Membro'
+
+export default (props) =>
+   <div>
+      <Membro nome='Carlos'  sobrenome='Silva'/>
+      <Membro nome='Roberto' sobrenome='Silva'/>
+      <Membro nome='Rafaela' sobrenome='Silva'/>
+   </div>
