57e5f5abcd4b710fc9b40fa18c1b1a318367a6b7
Renato Caminha Juaçaba Neto <rcaminha@etu.unistra.fr>
2019-07-04 17:29:20 +0200

    Filhos de componentes

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponent from './components/PrimeiroComponent'
 // import CompA, {CompB as B} from './components/DoisComponents'
 // import MultiElementos from './components/MultiElementos'
-import FamiliaSilva from './components/FamiliaSilva'
-
+// import FamiliaSilva from './components/FamiliaSilva'
+import Familia from './components/Familia'
+import Membro from './components/Membro'
 const root = document.getElementById('root')
 
 ReactDOM.render(
@@ -13,5 +14,14 @@ ReactDOM.render(
       {/* <CompA valor='Sou A'/> */}
       {/* <B valor='Sou B'/> */}
       {/*<MultiElementos/> */}
-      <FamiliaSilva/>
+      {/* <FamiliaSilva/> */}
+      <Familia>
+         <Membro nome='Carlos' sobrenome='Roberto'/>
+         <Membro nome='Carla' sobrenome='Roberto'/>
+      </Familia>
+
+      <Familia>
+         <Membro nome='Felicia' sobrenome='Arruda'/>
+         <Membro nome='Roberta' sobrenome='Arruda'/>
+      </Familia>
    </div>, root)
