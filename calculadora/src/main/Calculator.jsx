10ed85e466dcf363699aa56dbe07076f247c946d
Renato Caminha Juaçaba Neto <rcaminha@etu.unistra.fr>
2019-07-09 17:05:59 +0200

    Background da calculadora

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import './Calculator.css'
+
+export default class Calculadora extends Component {
+   render() {
+      return (
+         <div className="calculator">
+         </div>
+      )
+   }
+}
