d0aa1554ff8940284fff13995f3ff80abf4d07ed
Renato Caminha Juaçaba Neto <rcaminha@etu.unistra.fr>
2019-07-17 16:34:16 +0200

    Segundo exemplo react redux

--- exercicios-react/src/index.jsx
+++ exercicios-react/src/index.jsx
@@ -11,14 +11,17 @@ import ReactDOM from 'react-dom'
 // import ComponentClasse from './components/ComponenteClasse'
 // import Contador from './components/Contador'
 // import Hook from './components/Hook'
-import Field from './components/Field'
-import FieldReducer from './components/FieldReducer'
+// import Field from './components/Field'
+// import FieldReducer from './components/FieldReducer'
+import Counter from './components/Counter'
+import CounterReducer from './components/CounterReducer'
 
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 const reducers = combineReducers({
-   field: FieldReducer
+   // field: FieldReducer
+   counter: CounterReducer
 })
 
 const root = document.getElementById('root')
@@ -51,6 +54,7 @@ ReactDOM.render(
       {/* <Contador/> */}
 
       {/* <Hook/> */}
-      <Field />
+      {/* <Field /> */}
+      <Counter />
       {/* </div> */}
    </Provider>, root)
